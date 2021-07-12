49155031cdf41bd5f6ab4e59722048e95f2676ee
Raphael <rapha.saverys@gmail.com>
2021-07-12 16:10:05 +0200

    Setup project (react) #5

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import AppRouter from "./Routes/AppRouter";
+
+function App() {
+  return (
+    <Fragment>
+      <AppRouter />
+    </Fragment>
+  );
+}
+
+export default App;
