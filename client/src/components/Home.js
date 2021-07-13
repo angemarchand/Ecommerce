5ee2852c64d3fdb90fe8e5abf75546da1b03408d
Raphael <rapha.saverys@gmail.com>
2021-07-13 12:29:16 +0200

    3: setting up Scss params AND begin the navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react'
+import Navbar from './Navbar'
 
 export default function Home() {
     return (
         <Fragment>
+            <Navbar />
             <h1>test</h1>
         </Fragment>
     )
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from "../assets/img/logo.png"
+import cart from "../assets/img/shopping_cart_black_24dp.svg"
+import search from "../assets/img/search_black_24dp.svg"
+import profile from "../assets/img/account_circle_black_24dp.svg"
+
+export default function Navbar() {
+    return (
+        <div id="navbar">
+            <img src={logo} className="logo"></img>
+            <div className="navLinks">
+                <a href="#">PORTABLE</a>
+                <a href="#">ACCESSOIRES</a>
+                <a href="#">PERIPHERIQUES</a>
+                <a href="#">CUSTOM</a>
+                <a href="#">COMMUNAUTÉ</a>
+            </div>
+            <img src={search}></img>
+            <img src={cart}></img>
+            <img src={profile}></img>
+        </div>
+    )
+}
