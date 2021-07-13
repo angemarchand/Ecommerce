8cefa7532c4ae448e74fba9ea451f8126e9f20b9
Raphael <rapha.saverys@gmail.com>
2021-07-13 14:43:49 +0200

    3: navbar component create

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import logo from "../assets/img/logo.png"
-import cart from "../assets/img/shopping_cart_black_24dp.svg"
-import search from "../assets/img/search_black_24dp.svg"
-import profile from "../assets/img/account_circle_black_24dp.svg"
+import React from "react";
+import logo from "../assets/img/logo.png";
+import cart from "../assets/img/shopping_cart_black_24dp.svg";
+import search from "../assets/img/search_black_24dp.svg";
+import profile from "../assets/img/account_circle_black_24dp.svg";
 
 export default function Navbar() {
-    return (
-        <div id="navbar">
-            <img src={logo} className="logo"></img>
-            <div className="navLinks">
-                <a href="#">PORTABLE</a>
-                <a href="#">ACCESSOIRES</a>
-                <a href="#">PERIPHERIQUES</a>
-                <a href="#">CUSTOM</a>
-                <a href="#">COMMUNAUTÉ</a>
-            </div>
-            <img src={search}></img>
-            <img src={cart}></img>
-            <img src={profile}></img>
-        </div>
-    )
+  return (
+    <div id="navbar">
+      <img src={logo} className="logo"></img>
+      <div className="navLinks">
+        <a href="#">PORTABLE</a>
+        <a href="#">ACCESSOIRES</a>
+        <a href="#">PERIPHERIQUES</a>
+        <a href="#">CUSTOM</a>
+        <a href="#">COMMUNAUTÉ</a>
+      </div>
+      <div className="navIcons">
+        <img src={search}></img>
+        <img src={cart}></img>
+        <img src={profile}></img>
+      </div>
+    </div>
+  );
 }
