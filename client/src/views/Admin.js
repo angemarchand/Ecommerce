e7b58b32a5c4ecabbaaf0dc1c53696aa7bb5153a
bastien <bastien.salis@epitech.eu>
2021-07-20 20:42:34 +0200

    admin graphical redesign ok/separate file for fetch

--- client/src/views/Admin.js
+++ client/src/views/Admin.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import AdminProduct from "../components/admin/Product";
+import AdminProductLine from "../components/admin/ProductLine";
+import ProductModal from "../components/admin/ProductModal"
+import {GETProducts} from "../api/Products"
 
 const Admin = () => {
 
@@ -7,26 +9,14 @@ const Admin = () => {
 
     useEffect(() => {
         async function getData() {
-            await getProducts();
+            setProducts(await GETProducts());
         }
         getData();
     }, [])
 
-    const getProducts = async () => {
-        const response = await fetch('https://localhost:8000/api/products', {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        })
-        const products = await response.json();
-        setProducts(products);
-        console.log(products);
-    }
-
     return (
         <div className="container-fluid">
+            <ProductModal />
             <table className="table">
                 <thead>
                     <tr>
@@ -35,8 +25,8 @@ const Admin = () => {
                         <th scope="col">PRICE</th>
                         <th scope="col">STOCK</th>
                         <th className="col-button" scope="col">
-                            <button type="button" className="btn btn-lg btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                modifier
+                            <button type="button" className="btn btn-lg btn-success" data-bs-toggle="modal" data-bs-target="#add-product">
+                                Ajouter
                             </button>
                         </th>
                         <th scope="col"></th>
@@ -44,7 +34,16 @@ const Admin = () => {
                 </thead>
                 <tbody>
                     {products ?
-                        products.map(item => <AdminProduct key={item.id} id={item.id} name={item.name} description={item.description} price={item.price} created_at={item.created_at} modified_at={item.modified_at} stock={item.stock} />)
+                        products.map(item => <AdminProductLine
+                            key={item.id}
+                            id={item.id}
+                            name={item.name}
+                            description={item.description}
+                            price={item.price}
+                            created_at={item.created_at}
+                            modified_at={item.modified_at}
+                            stock={item.stock}
+                        />)
                         :
                         <tr><th><p>Aucun produit enregistré</p></th></tr>}
                 </tbody>
