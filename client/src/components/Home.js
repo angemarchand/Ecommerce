c49b36a2d874349259515489c046f6dd91969648
AymericN <aymeric.navet@epitech.eu>
2021-07-21 17:52:07 +0200

    login logout

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,26 @@
 import React, { Fragment } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import axios from 'axios';
+
 
 export default function Home() {
+  
+  const config = {
+    headers:{
+      Authorization: 'Bearer' + localStorage.getItem('token')
+    }
+  }
+
+  axios.get('user', config).then(
+    res => {
+      console.log(res);
+    },
+    err => {
+      console.log(err);
+    }
+  )
+
   return (
     <Fragment>
       <Navbar />
