622ffc8f4ef80b319c84284a2f3aa0eb422573f0
Clementmrz <clement.murzeau@epitech.eu>
2021-07-23 12:15:29 +0200

    commit fonctionnel

--- client/src/api/Pictures.js
+++ client/src/api/Pictures.js
@@ -2,9 +2,9 @@ export const POSTPicture = async (tabName, id_product, tabImagesB64) => {
     
     let i = 0;
     let product = [];
-    // for (let element of tabName)
-    // {
-        console.log(`name:${tabName[0]}, id_product:${parseInt(id_product)}, alt:${null}, image_b64:${tabImagesB64[i]}`);
+    for (let element of tabName)
+    {
+        console.log(`name:${element}, id_product:${parseInt(id_product)}, alt:${null}, image_b64:${tabImagesB64[i]}`);
         const response = await fetch('https://localhost:8000/api/pictures', {
             method: "POST",
             headers: {
@@ -12,15 +12,15 @@ export const POSTPicture = async (tabName, id_product, tabImagesB64) => {
                 'Content-Type': 'application/json; charset=UTF-8',
             },
             body: JSON.stringify({
-                name: tabName[0],
-                id_product: parseInt(id_product),
+                name: element,
+                idProduct: parseInt(id_product),
                 alt: null,
-                image_b64: tabImagesB64[i]
+                imageB64: tabImagesB64[i]
             })
         })
-    //     product.push(await response.json());
-    //     i++;
-    // };
+        product.push(await response.json());
+        i++;
+    };
     
     return product;
 }
