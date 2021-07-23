a5b6cb4bb36bc36299c838fa73c0c2536c4aff24
bastien <bastien.salis@epitech.eu>
2021-07-23 18:07:07 +0200

    use bootstrap for responsive navbar + delete unused css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,21 +6,35 @@ import logo from "../assets/img/logo.png";
 function Navbar() {
 
     return (
-        <div id="navbar">
-            <div className="logo">
-                <img src={logo} alt="logo" />
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container-xl">
+                <Link to="/" className="navbar-brand logo">
+                    <img src={logo} alt="logo" />
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/"> Home </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/"> Produits </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/users/register"> Register </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/users/login"> Login </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="#"> Logout </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-
-            <div>
-                <ul>
-                    <Link to="/"> Home </Link>
-                    <Link to="/products"> Produits </Link>
-                    <Link to="/users/register"> Register </Link>
-                    <Link to="/users/login"> Login </Link>
-                    <Link to="/"> Logout </Link>
-                </ul>
-            </div>
-        </div>
+        </nav>
     )
 }
 
