7c9bfe09781aabfa3e279a3973b6bd0673237886
Clementmrz <clement.murzeau@epitech.eu>
2021-07-26 10:39:37 +0200

    ajout page home (oublie de switche de branche... ça arrive...)

---
+++ client/src/views/Home.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import CardProduct from "../components/CardProduct";
+import Footer from "../components/Footer";
+import { GETProducts } from "../services/api/Products";
+
+function ListingProducts() {
+
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      await getProducts();
+    }
+    getData();
+
+  }, [])
+
+  const getProducts = async () => {
+    const products = await GETProducts();
+    setProducts(products);
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <div className="container-fluid container-lg" style={{ border: "soid" }}>
+        <div className="row">
+            {
+              products ?
+                products.map(items => <div className="col"><CardProduct name={items.name} description={items.description} price={items.price} /></div>)
+                :
+                null
+            }
+        </div>
+      </div>
+      {/* <div className="containerProducts">
+        {
+          products ?
+            products.map(items => <CardProduct name={items.name} description={items.description} price={items.price} />)
+            :
+            null
+        }
+      </div> */}
+      <Footer />
+    </div>
+  );
+}
+
+
+export default ListingProducts;
